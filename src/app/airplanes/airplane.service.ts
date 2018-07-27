c8567f56fd1961aba669251ffffa930a713c1f3b
Ivan Lysakov <ivanlysakov@gmail.com>
2018-07-27 12:17:08 +0300

    frontend finished

--- src/app/airplanes/airplane.service.ts
+++ src/app/airplanes/airplane.service.ts
@@ -18,7 +18,7 @@ export class AirplaneService {
   getAll(): Observable<Airplane[]> {
     return this.http.get<Airplane[]>(this.airplanesUrl);
   }
-  
+
   getById(id: number): Observable<Airplane> {
     const url = `${this.airplanesUrl}/${id}`;
     return this.http.get<Airplane>(url);
@@ -30,16 +30,15 @@ export class AirplaneService {
     return this.http.delete<Airplane>(url, httpOptions);
 
   }
-  create (airplane: Airplane): Observable<Airplane> {
+  create(airplane: Airplane): Observable<Airplane> {
     return this.http.post<Airplane>(this.airplanesUrl, airplane, httpOptions);
-        
   }
 
-  update (airplane: Airplane): Observable<any> {
+  update(airplane: Airplane): Observable<any> {
     const id = airplane.id;
     const url = `${this.airplanesUrl}/${id}`;
     return this.http.put(url, airplane, httpOptions);
-        
+
   }
 
 }
